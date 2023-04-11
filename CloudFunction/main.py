2505bb43b3c3635d1573ccb7e1505b6227e19fde
Juan José Cacho Toca <juanjo.cacho@evenbytes.com>
2023-04-11 11:29:19 +0200

    write to bigquery

--- CloudFunction/main.py
+++ CloudFunction/main.py
@@ -1,13 +1,31 @@
-def hello_world(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
-    name = request.args.get('name')
+from flask import escape
+from google.cloud import bigquery
+from datetime import datetime
 
-    return "Hello World! {name}"
+# Función que se ejecutará en la Cloud Function
+def write_to_bigquery(request):
+    # Recupera los parámetros desde la solicitud HTTP
+    dataset = escape(request.args.get('dataset'))
+    table = escape(request.args.get('table', default='temperature'))
+    temperature = escape(request.args.get('temperature'))
+    
+    # Obtiene la fecha y hora actual
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    
+    # Inicializa el cliente de BigQuery
+    client = bigquery.Client()
+    
+    # Configura la referencia a la tabla destino
+    table_ref = client.dataset(dataset).table(table)
+    
+    # Crea el objeto de registro que se escribirá en la tabla
+    data = {"temperature": temperature, "timestamp": timestamp}
+    
+    # Inserta el registro en la tabla
+    errors = client.insert_rows_json(table_ref, [data])
+    
+    # Si hay algún error al insertar los datos, devuelve un mensaje de error
+    if errors:
+        return f'Error al insertar los datos en BigQuery: {errors}', 500
+    else:
+        return 'Datos insertados correctamente en BigQuery.', 200
