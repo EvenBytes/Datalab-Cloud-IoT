ea652eee567290ba32173d97437991ae34defe1d
Juan José Cacho Toca <juanjo.cacho@evenbytes.com>
2023-04-11 11:50:02 +0200

    Changes script

--- CloudFunction/main.py
+++ CloudFunction/main.py
@@ -2,15 +2,15 @@ from flask import escape
 from google.cloud import bigquery
 from datetime import datetime
 
-#gcloud functions deploy <nombre_de_la_funcion> --runtime python37 --trigger-http --region <region> --entry-point <nombre_de_la_funcion> --source <directorio_de_la_funcion> --requirements <archivo_requirements.txt>
+#gcloud functions deploy <nombre_de_la_funcion> --runtime python37 --trigger-http --entry-point <nombre_de_la_funcion> --source <directorio_de_la_funcion> --requirements <archivo_requirements.txt>
 
 
 # Función que se ejecutará en la Cloud Function
 def write_to_bigquery(request):
     # Recupera los parámetros desde la solicitud HTTP
-    dataset = escape(request.args.get('dataset'))
-    table = escape(request.args.get('table', default='temperature'))
-    temperature = escape(request.args.get('temperature'))
+    dataset = escape(request.args.get('dataset', default='sensorHubData'))
+    table = escape(request.args.get('table', default='Temperatures'))
+    temperature = escape(request.args.get('data', default=0.00))
     
     # Obtiene la fecha y hora actual
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
@@ -22,7 +22,7 @@ def write_to_bigquery(request):
     table_ref = client.dataset(dataset).table(table)
     
     # Crea el objeto de registro que se escribirá en la tabla
-    data = {"temperature": temperature, "timestamp": timestamp}
+    data = {"temperature": temperature, "timestamp_temperature": timestamp}
     
     # Inserta el registro en la tabla
     errors = client.insert_rows_json(table_ref, [data])
